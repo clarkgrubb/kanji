fbbe24e308cedb28af2603257cf56f37392fc83e
Clark Grubb <clarkgrubb@gmail.com>
2022-11-22 08:21:40 -0800

    hebrew

---
+++ src/hebrew.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import os
+import re
+import sys
+
+DEFAULT_INPUT = os.path.expanduser('~/Source/clarkgrubb/wikidot/hebrew.txt')
+
+def hebrew(path):
+    with open(path) as f:
+        index = ''
+        character = ''
+        for line in f:
+            parts = line.rstrip().split('||')
+            if len(parts) != 5:
+                sys.stderr.write(f'WARNING bad line {line}')
+            else:
+                hebrew = parts[1]
+                english = parts[2]
+                chapter = parts[3]
+                sys.stdout.write('||')
+                sys.stdout.write('||'.join([chapter, english, hebrew]))
+                sys.stdout.write('||\n')
+
+
+if __name__ == '__main__':
+    hebrew(DEFAULT_INPUT if len(sys.argv) < 2 else sys.argv[1])
